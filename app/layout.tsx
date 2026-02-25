9532e50f728863d901454911eeeac33fa2db3a38
arpan7sarkar <arpansakar@gmail.com>
2026-02-25 13:29:36 +0530

    implemented basic landing page/loading home

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono, Noto_Sans } from "next/font/google";
 import "./globals.css";
 import { ConvexClientProvider } from "@/components/providers/convex-provider";
 import { ClerkProvider } from "@clerk/nextjs";
+import { ThemeProvider } from "@/components/providers/theme-provider";
 
 const notoSans = Noto_Sans({ variable: '--font-sans' });
 
@@ -28,13 +29,26 @@ export default function RootLayout({
 }>) {
   return (
     <ClerkProvider dynamic>
-      <html lang="en" className={notoSans.variable}>
+      <html lang="en" className={notoSans.variable} suppressHydrationWarning>
         <body
           className={`${geistSans.variable} ${geistMono.variable} antialiased`}
         >
-          <ConvexClientProvider>{children}</ConvexClientProvider>
+          <ConvexClientProvider>
+            <ThemeProvider
+              attribute="class"
+              defaultTheme="dark"
+              enableSystem
+              disableTransitionOnChange
+            >
+              {children}
+            </ThemeProvider>
+
+          </ConvexClientProvider>
         </body>
       </html>
     </ClerkProvider>
   );
 }
+
+
+
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,29 @@
-import { ComponentExample } from "@/components/component-example";
+"use client";
 
-export default function Page() {
-    return <ComponentExample />;
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
+
+
+export default function Home() {
+    return (
+        <main className="flex min-h-screen flex-col items-center justify-center bg-[#00A884] dark:bg-[#111B21] text-white p-4">
+            <div className="text-center space-y-6">
+                <h1 className="text-4xl font-bold sm:text-5xl md:text-6xl">
+                    Welcome to Whatsapp
+                </h1>
+                <p className="text-xl md:text-2xl text-white/80" >
+                    It's a clone of Whatsapp
+                </p>
+                <Button asChild size="lg" className="px-15 py-7 bg-white text-[#00A884] hover:bg-gray-100 dark:bg-[#202C33] dark:text-white dark:hover:bg-[#243942]
+                text-xl
+                ">
+                    <Link className=" font-semibold " href="/chat">
+                        Start Chatting</Link>
+
+                </Button>
+            </div>
+        </main>
+    );
 }
+/*"email":"arpansakar@gmail.com","name":"Arpan","profileImage */
